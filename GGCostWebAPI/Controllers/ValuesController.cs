4747a2705e48244ecc3c935304206470333595c7
YoungChulShin <go1323@gmail.com>
2017-12-30 17:26:37 +0900

    공감 가계부 Web API 최초 추가

---
+++ GGCostWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GGCostWebAPI.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GGCostWebAPI.Controllers
+{
+    [Route("api/test")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            GoogleDrive googleDrive = new GoogleDrive();
+            googleDrive.GetFileList();
+            System.Threading.Thread.Sleep(5000);
+            return new string[] { "Fender", "Gibson" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            System.Threading.Thread.Sleep(6000);
+            return $"The value you sent was: {id} ";
+        }
+    }
+}
