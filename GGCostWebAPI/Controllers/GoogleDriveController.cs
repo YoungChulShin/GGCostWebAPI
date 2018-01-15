4aa3d17f4269a41b77ce80bc73ef7efcce6137ca
YoungChulShin <go1323@gmail.com>
2018-01-16 06:55:36 +0900

    Add google drvice save function

---
+++ GGCostWebAPI/Controllers/GoogleDriveController.cs
@@ -0,0 +1,56 @@
+﻿using GGCostWebAPI.Handlers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Net;
+
+namespace GGCostWebAPI.Controllers
+{
+    [Route("api/googledrive")]
+    public class GoogleDriveController : Controller
+    {
+        GoogleDrive mGoogleDrive = new GoogleDrive();
+
+        // GET api/googledrive
+        [HttpGet]
+        public byte[] Get()
+        {
+            return mGoogleDrive.LoadFile();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            try
+            {
+                var form = Request.Form;
+                if (form.Files.Count == 0)
+                {
+                    throw new FileNotFoundException("업로드할 파일을 찾지 못했습니다");
+                }
+
+                foreach (var formFile in form.Files)
+                {
+                    if (formFile.Length > 0)
+                    {
+                        using (var ms = new MemoryStream())
+                        {
+                            formFile.CopyTo(ms);
+                            mGoogleDrive.SaveFile(ms.ToArray());
+                        }
+                    }
+                }
+
+                return StatusCode((int)HttpStatusCode.OK, "OK");
+            }
+            catch (FileNotFoundException ex)
+            {
+                return StatusCode((int)HttpStatusCode.NoContent, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
--- GGCostWebAPI/Handlers/GoogleDrive.cs
+++ GGCostWebAPI/Handlers/GoogleDrive.cs
@@ -1,22 +1,106 @@
 ﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Download;
 using Google.Apis.Drive.v3;
 using Google.Apis.Drive.v3.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace GGCostWebAPI.Handlers
 {
     public class GoogleDrive
     {
-        static string[] Scopes = { DriveService.Scope.Drive };
-        static string ApplicationName = "Drive API Quickstart";
-
+        private string mFileName = "GGCostData.db";
         private DriveService mService = null;
+
+        public void SaveFile(byte[] aFileData)
+        {
+            var dbDataFile = GetDBDataFromDrive(mFileName);
+            if (dbDataFile == null)
+            {
+                var folder = GetDBDataFromDrive("공감가계부");
+                var folderID = (folder == null) ? CreateFolder() : folder.Id;
+                CreateFile(aFileData, folderID);
+            }
+            else
+            {
+                UpdateFile(aFileData, dbDataFile.Id);
+            }
+        }
+        public byte[] LoadFile()
+        {
+            var dbDataFile = GetDBDataFromDrive(mFileName);
+            if (dbDataFile == null)
+            {
+                throw new Exception("Error : Saved file is not exists");
+            }
+
+            return DownloadFile(dbDataFile.Id);
+        }
+
+        private string CreateFolder()
+        {
+            var service = GetService();
+
+            var fileMetadata = new File()
+            {
+                Name = "공감가계부",
+                MimeType = "application/vnd.google-apps.folder"
+            };
+            var request = service.Files.Create(fileMetadata);
+            request.Fields = "id";
+            var file = request.Execute();
+
+            return file.Id;
+        }
+        private void CreateFile(byte[] aFileData, string aFolderID)
+        {
+            var service = GetService();
+
+            File body = new File();
+            body.Name = mFileName;
+            body.Description = "공감 가계부 데이터 파일";
+            body.MimeType = GetMimeType();
+            body.Parents = new List<string> { aFolderID };
+
+            using (System.IO.MemoryStream stream = new System.IO.MemoryStream(aFileData))
+            {
+                FilesResource.CreateMediaUpload request = service.Files.Create(body, stream, GetMimeType());
+                request.Upload();
+                var result = request.ResponseBody;
+            }
+        }
+        private void UpdateFile(byte[] aFileData, string aFileID)
+        {
+            var service = GetService();
+
+            File body = new File();
+            body.Name = mFileName;
+            body.Description = "공감 가계부 데이터 파일";
+            body.MimeType = GetMimeType();
+
+            using (System.IO.MemoryStream stream = new System.IO.MemoryStream(aFileData))
+            {
+                FilesResource.UpdateMediaUpload request = service.Files.Update(body, aFileID, stream, GetMimeType());
+                request.Upload();
+                var result = request.ResponseBody;
+            }
+        }
+        private byte[] DownloadFile(string aFileID)
+        {
+            var service = GetService();
+            var reqeust = service.Files.Get(aFileID);
+            byte[] fileData;
+            using (var stream = new System.IO.MemoryStream())
+            {
+                reqeust.Download(stream);
+                fileData = stream.ToArray();
+            }
+            
+            return fileData;
+        }
 
         private DriveService GetService()
         {
@@ -35,85 +119,27 @@ namespace GGCostWebAPI.Handlers
 
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
                     GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
+                    new string[] { DriveService.Scope.Drive },
                     "user",
                     CancellationToken.None,
                     new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
             }
 
             // Create Drive API service.
             var service = new DriveService(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
+                ApplicationName = "GGCost",
             });
 
             return service;
         }
-
-        public void UpdateFile(string aFilePath)
-        {
-            var service = GetService();
-            var fileMetaData = new File()
-            {
-                Name = "db.sqlite"
-            };
-            FilesResource.CreateMediaUpload reqeust;
-            
-            using (var uploadStream = new System.IO.FileStream(aFilePath,
-                                                               System.IO.FileMode.Open,
-                                                               System.IO.FileAccess.Read))
-            {
-                reqeust = service.Files.Create(
-                    fileMetaData, uploadStream, GetMimeType(aFilePath));
-                reqeust.Upload();
-                var file = reqeust.ResponseBody;
-            }
-        }
-
-        public void InsertFiles(string aFilePath)
-        {
-            var service = GetService();
-
-            File body = new File();
-            body.Name = System.IO.Path.GetFileName(aFilePath);
-            body.Description = "File uploaded by Diamto Drive Sample";
-            body.MimeType = GetMimeType(aFilePath);
-            body.Parents = new List<string> { "root" };
-
-            // File's content.
-            byte[] byteArray = System.IO.File.ReadAllBytes(aFilePath);
-            System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
-            try
-            {
-                FilesResource.CreateMediaUpload request = service.Files.Create(body, stream, GetMimeType(aFilePath));
-                request.Upload();
-                var result = request.ResponseBody;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("An error occurred: " + e.Message);
-                
-            }
-        }
-
-        private string GetMimeType(string aFilePath)
-        {
-            string mimeType = "application/unknown";
-            string ext = System.IO.Path.GetExtension(aFilePath).ToLower();
-            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
-            if (regKey != null && regKey.GetValue("Content Type") != null)
-                mimeType = regKey.GetValue("Content Type").ToString();
-            return mimeType;
-        }
-        
-        public void GetDBDataFromDrive()
+        private File GetDBDataFromDrive(string aName)
         {
             var service = GetService();
             // Define parameters of request.
             FilesResource.ListRequest listRequest = service.Files.List();
-            listRequest.Q = "name = 'GGCostData'";
+            listRequest.Q = $"name = '{aName}'";
             listRequest.PageSize = 1;
             listRequest.Fields = "nextPageToken, files(id, name)";
 
@@ -122,22 +148,21 @@ namespace GGCostWebAPI.Handlers
             Console.WriteLine("Files:");
             if (files != null && files.Count > 0)
             {
-                foreach (var file in files)
-                {
-                    Console.WriteLine("{0} ({1})", file.Name, file.Id);
-                }
+                return files[0];
             }
             else
             {
-                Console.WriteLine("No files found.");
+                return null;
             }
-            Console.Read();
-
         }
-
-        public void SaveDBDataToDrive()
+        private string GetMimeType()
         {
-
+            string mimeType = "application/unknown";
+            string ext = "sqlite";
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
+            if (regKey != null && regKey.GetValue("Content Type") != null)
+                mimeType = regKey.GetValue("Content Type").ToString();
+            return mimeType;
         }
     }
 }
